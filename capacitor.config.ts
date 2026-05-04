ea61bca8af65b2448ad97f2131811b0fbf128bb7
Cleymer Avila <avilacleymer05@gmail.com>
2026-05-03 22:22:35 -0500

    feat: splash screen and icon implementation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'SteamyApp',
-  webDir: 'www'
+  webDir: 'www',
+   plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#F8312F",
+      showSpinner: false,
+      androidSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
